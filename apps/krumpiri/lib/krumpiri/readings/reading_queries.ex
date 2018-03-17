be74be4fa88f295365c193305b2675e68730f369
zoltanmaric <zoltanmaric@git.com>
2018-03-17 14:40:03 +0100

    insert from json api into cassandra, timestamp encoding doesn't work

--- apps/krumpiri/lib/krumpiri/readings/reading_queries.ex
+++ apps/krumpiri/lib/krumpiri/readings/reading_queries.ex
@@ -5,4 +5,23 @@ defmodule Krumpiri.ReadingQueries do
     statement = "INSERT INTO readings (id, net_in_wh, read_time, signature, username) VALUES (uuid(), 13, '2017-01-01 12:00:00', 'signature', 'username')"
     {:ok, %Xandra.Void{}} = Xandra.execute(conn, statement, _params = [])
   end
+
+#{"net_in_wh": 12, "read_time": "2018-03-16 12:00:00", "signature": "potpis", "username": "zokica"}
+
+  def insert(reading) do
+    {:ok, conn} = Xandra.start_link(after_connect: &Xandra.execute(&1, "USE Krumpiri"))
+
+    query = "INSERT INTO readings (id, net_in_wh, read_time, signature, username)
+      VALUES (uuid(), :net_in_wh, :read_time, :signature, :username)"
+    prepared = Xandra.prepare!(conn, query)
+
+    params = %{
+      "net_in_wh" => reading["net_in_wh"],
+      "read_time" => reading["read_time"],
+      "signature" => reading["signature"],
+      "username" => reading["username"]
+    }
+
+    Xandra.execute!(conn, prepared, params)
+  end
 end
--- apps/krumpiri_web/lib/krumpiri_web/controllers/readings_controller.ex
+++ apps/krumpiri_web/lib/krumpiri_web/controllers/readings_controller.ex
@@ -1,14 +1,14 @@
 defmodule KrumpiriWeb.ReadingsController do
   use KrumpiriWeb, :controller
 
-  def submit(conn, %{"_json" => json}) do
-    IO.puts("json: #{json}")
-    json(conn, "")
-  end
+  alias Krumpiri.ReadingQueries
 
-  def submit(conn, params) do
-    IO.puts("params")
-    IO.inspect(params)
+  def submit(conn, reading) do
+    IO.write("received json: ")
+    IO.inspect(reading)
+
+    ReadingQueries.insert(reading)
+
     json(conn, "")
   end
 
