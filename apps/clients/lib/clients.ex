0d9d5e161990fdb851a9f10c6c0d7b4ea9a69405
zoltanmaric <zoltanmaric@git.com>
2018-03-18 16:02:08 +0100

    create bare clients application

---
+++ apps/clients/lib/clients.ex
@@ -0,0 +1,18 @@
+defmodule Clients do
+  @moduledoc """
+  Documentation for Clients.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Clients.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
