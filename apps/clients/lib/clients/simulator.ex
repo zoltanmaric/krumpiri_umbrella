e1d59180920cb1373d1317bceb5d1e219380eafb
zoltanmaric <zoltanmaric@git.com>
2018-03-19 23:00:44 +0100

    report readings to api, not parallel yet

---
+++ apps/clients/lib/clients/simulator.ex
@@ -0,0 +1,32 @@
+defmodule Clients.Simulator do
+  @moduledoc false
+
+  alias Clients.SmartMeter
+
+  def report_readings(num_clients, interval_ms, num_readings) do
+    1..num_clients
+    |> Enum.map(&generate_and_report(&1, interval_ms, num_readings))
+  end
+
+  defp generate_and_report(user_index, interval_ms, num_readings) do
+    SmartMeter.generate_readings(round(:rand.uniform() * 1000), interval_ms, num_readings)
+    |> Stream.map(&to_json(user_index, &1))
+    |> Stream.map(&send/1)
+    |> Stream.run
+  end
+
+  defp to_json(user_index, net_in_wh) do
+    Poison.encode! %{
+      "net_in_wh" => net_in_wh,
+      "read_time" => DateTime.utc_now(),
+      "signature" => "signature",
+      "username" => "user_#{user_index}"
+    }
+  end
+
+  defp send(reading) do
+    HTTPoison.post!("http://localhost:4000/api/submit", reading, [{"Content-Type", "application/json"}])
+  end
+
+end
+
--- apps/clients/lib/clients/smart_meter.ex
+++ apps/clients/lib/clients/smart_meter.ex
@@ -1,20 +1,17 @@
 defmodule Clients.SmartMeter do
   @moduledoc false
 
-  def stream_readings(initial_delay_ms, interval_ms, num_readings) do
+  def generate_readings(initial_delay_ms, interval_ms, num_readings) do
     first_tick = Stream.timer(initial_delay_ms)
     subsequent_ticks = Stream.interval(interval_ms)
 
     Stream.concat(first_tick, subsequent_ticks)
-    |> Stream.map(fn _ -> random_reading end)
+    |> Stream.map(fn _ -> random_delta_reading end)
     |> Stream.scan(0, &(&1 + &2))
-    |> Stream.each(&IO.puts/1)
-    |> Enum.take(num_readings)
+    |> Stream.take(num_readings)
   end
 
-  defp random_reading() do
-   :rand.normal()
+  defp random_delta_reading do
+   round(:rand.normal())
   end
-
-
 end
